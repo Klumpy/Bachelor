f3d610c45d0220497200a50d8d7e334a25f09c52
Daniel Krogstad <daniek08@uia.no>
2018-03-14 15:47:56 +0100

    basic infrastructure for a CAN-BUS sensor data transmitter

---
+++ canSend/canSend.ino
@@ -0,0 +1,35 @@
+// testset for send data on CAN-BUS
+
+#include <mcp_can.h>
+#include <SPI.h>
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+//sensor IDs
+const int hallEffect = 0x00;
+const int brakePressure = 0x01;
+const int mpu6050 = 0x02;
+const int rotaryEncoder = 0x03;
+
+void setup() {
+
+    Serial.begin(115200);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
+}
+
+void loop() {
+
+
+}
+
