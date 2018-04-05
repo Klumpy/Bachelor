9c6f236638d4b80b233bd7d8b2c451e0095c34ee
Theodor Fossum <theodf15@student.uia.no>
2018-04-05 10:42:02 +0200

    Tested the void setup, fixed some issues

--- PitStop/PitStop.ino
+++ PitStop/PitStop.ino
@@ -11,21 +11,23 @@ const byte txPin = 6;
 const int chipSelect = 4;   //Setting Chipselect to pin 4, this is for the SD-card
 String CANstring = "";      //A string for assembling the data we are logging
 char LoraSendBuff[4];       //Buffer we are sending to CarTransmitter
-uint8_t add = 1;       //The address in the buffer
-uint8_t chk = 2;       //The checksum of the buffer
-uint8_t len = 4;         //The length of the buffer
+uint8_t add = 0x1A;         //The address in the buffer 00011010
+uint8_t chk = 2;            //The checksum of the buffer
+uint8_t loraLen = 4;        //The length of the buffer
 uint8_t CANconfig = 10;     //The config that decides how many CAN messages we want
   
-LoraSendBuff[0] = add;
-LoraSendBuff[1] = chk;
-LoraSendBuff[2] = len;
-LoraSendBuff[3] = CANconfig;
+
 
 SoftwareSerial uart(rxPin, txPin);
 RH_RF95 lora(uart);
 
 void setup() {
 
+  configBuff[0] = add;
+  configBuff[1] = chk;
+  configBuff[2] = loraLen;
+  configBuff[3] = CANconfig;
+  
   Serial.begin(115200);
 
   //SD-card init
@@ -33,7 +35,7 @@ void setup() {
   {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.print("Initializing SD card");
+  Serial.println("Initializing SD card");
     
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) 
@@ -42,7 +44,7 @@ void setup() {
     return;
   }
   Serial.println("card initialized");
-  Serial.print("-----------------------------");
+  Serial.println("-----------------");
   delay (100);
   
   //LoRa init
@@ -54,26 +56,34 @@ void setup() {
     delay(100);
   }
   Serial.println("LoRa init ok!");
+  Serial.println("-----------------");
   lora.setFrequency(868.0);
-  Serial.print("-----------------------------");
 
-//wait for message from CarTransmitter
-Serial.print("Waiting for message from CarTransmitter"); 
-if(lora.available())
-  {  
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-    if(lora.recv(buf, &len))
+//wait for Setupmessage from CarTransmitter
+  Serial.println("Waiting for Setupmessage from CarTransmitter"); 
+  while(!lora.available())
+  {
+    Serial.println("Message not available");
+    Serial.println("Trying again");
+    delay(300);
+  }  
+   uint8_t loraBufStp[RH_RF95_MAX_MESSAGE_LEN];
+   uint8_t recvLen = sizeof(loraBufStp);
+   if(lora.recv(loraBufStp, &recvLen))
     {
-     Serial.print("got request: ");
-     Serial.println((char*)buf); 
+      Serial.println("-----------------"); 
+      Serial.println("Got request: ");
+      Serial.println(loraBufStp[0],HEX);
+      Serial.println(loraBufStp[1]); 
+      Serial.println(loraBufStp[2]); 
+      Serial.println(loraBufStp[3]); 
+      Serial.println("-----------------"); 
      
-     //Sender svar til CarTransmitter med config
-      lora.send(LoraSendBuff, len);
+      //Sender svar til CarTransmitter med config
+      lora.send(configBuff, loraLen);
       lora.waitPacketSent();
       Serial.println("Sent a reply");
-    } 
-  }
+    }
 }
 
 void loop() {
@@ -83,8 +93,8 @@ Serial.print("Waiting for message from CarTransmitter");
 if(lora.available())
 { 
    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-   uint8_t len = sizeof(buf);
-   lora.recv(buf, &len);
+   uint8_t recvLen = sizeof(buf);
+   lora.recv(buf, &recvLen);
    
   if (buf[0] != add)   //Checking if the address recieved is the same as address sent 
   {
@@ -99,12 +109,17 @@ if(lora.available())
       if(lora.available())
       {
         uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-        uint8_t len = sizeof(buf);
-        lora.recv(buf, &len);
-        if(lora.recv(buf, &len))
+        uint8_t recvLen = sizeof(buf);
+        lora.recv(buf, &recvLen);
+        if(lora.recv(buf, &recvLen))
             {
-              Serial.print("Got Payload: ");
-              Serial.println((char*)buf);
+              Serial.println("-----------------"); 
+              Serial.println("Got request: ");
+              Serial.println(loraBufStp[0],HEX);
+              Serial.println(loraBufStp[1]); 
+              Serial.println(loraBufStp[2]); 
+              Serial.println(loraBufStp[3]); 
+              Serial.println("-----------------");
             }
           }
         }
