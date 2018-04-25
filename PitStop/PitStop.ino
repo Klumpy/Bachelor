17de0210811d28d207213cb7e19c5b3e740adea2
Theodor Fossum <theodf15@student.uia.no>
2018-04-25 12:16:56 +0200

    Added code to fix the address cehck

--- PitStop/PitStop.ino
+++ PitStop/PitStop.ino
@@ -28,22 +28,23 @@ void setup() {
   configBuf[1] = chk;
   configBuf[2] = loraLen;
   configBuf[3] = CANconfig;
+  recvLen = sizeof(loraBufStp);
   
   Serial.begin(115200);
 
   // SD-card init
   while (!Serial) 
-  {
-    ; // Wait for serial port to connect. Needed for native USB port only
-  }
+        {
+          ; // Wait for serial port to connect. Needed for native USB port only
+        }
   Serial.println(F("Initializing SD card"));
     
   // See if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) 
-  {
-    Serial.println(F("Card failed, or not present"));
-    while(1);
-  }
+     {
+       Serial.println(F("Card failed, or not present"));
+       while(1);
+     }
   Serial.println(F("SD-card initialized"));
   Serial.println(F("-----------------"));
   delay (100);
@@ -51,95 +52,97 @@ void setup() {
   // LoRa init
   Serial.println(F("LoRa reciever init ok!"));
   while (!lora.init())
-  {
-    Serial.println(F("LoRa initialized failed"));
-    Serial.println(F("Initializing LoRa again"));
-    delay(100);
-  }
+        {
+          Serial.println(F("LoRa initialized failed"));
+          Serial.println(F("Initializing LoRa again"));
+          delay(100);
+        }
   Serial.println(F("LoRa initialized ok!"));
   Serial.println(F("-----------------"));
   lora.setFrequency(868.0);                      // Setting the frequency to 868 MHz
 
   Serial.println(F("Waiting for Setupmessage")); // wait for Setupmessage from CarTransmitter
-  while(!lora.available())
-  {
-    Serial.println(F("Message not available"));
-    Serial.println(F("Trying again"));
-    delay(1000);
-  }   
-  //loraBufStp[RH_RF95_MAX_MESSAGE_LEN];
-  recvLen = sizeof(loraBufStp);
- 
-  if(lora.recv(loraBufStp, &recvLen))       
-    {
-         if(loraBufStp[0] != add)                        // Checking if the address recieved is the same as address sent 
-            {
-              Serial.println(F("Got the wrong address"));
-              Serial.println(F("Trying again"));
-            } 
-            else
-            {
-              Serial.println(F("-----------------")); 
-              Serial.println(F("Got message: "));
-              for(int i = 0; i < recvLen; i++)          // for loop that is printing the buffer we got
+
+  while(loraBufStp[0] != add)
+  { 
+   if(lora.available())
+     {          
+       if(lora.recv(loraBufStp, &recvLen))       
+         {
+            if(loraBufStp[0] == add)                        // Checking if the address recieved is the same as address sent 
               {
-                Serial.print("Position ");
-                Serial.print(i);
-                Serial.print(" of the buffer is: ");
-                Serial.println(loraBufStp[i], HEX);
+                lora.send(configBuf, loraLen);           // Sending reply to CarTransmitter with the config
+                lora.waitPacketSent();
+                Serial.println(F("Sent a reply"));           
+              } 
+              else
+              {
+                Serial.println(F("Got the wrong address"));
+                Serial.println(F("Trying again"));
               }
-              lora.send(configBuf, loraLen);           // Sending repl to CarTransmitter with the config
-              lora.waitPacketSent();
-              Serial.println(F("Sent a reply"));
-            }
+         }
+     }
+     else
+     {
+       Serial.println(F("Message not available"));
+       Serial.println(F("Trying again"));
+       delay(1000);
+     }
+  }  
+  Serial.println(F("-----------------")); 
+  for(int i = 0; i < recvLen; i++)          // for-loop that is printing the buffer we got
+     {
+       Serial.print("Position ");
+       Serial.print(i);
+       Serial.print(" of the buffer is: ");
+       Serial.println(loraBufStp[i], HEX);
      }
 }
 
 void loop() {
 
-File canDataLog = SD.open("canData.txt", FILE_WRITE);           // Opens the file
+File canDataLog = SD.open("canData.txt", FILE_WRITE);           // Opens the canData.txt file
 delay(100);
+
 //Recieving CAN-packages
 if(lora.available() && canDataLog)
   {
-    Serial.println(F("LoRa is available"));
-    Serial.println(F("The file is also available"));
-   
-   // Collecting the recieved data in a buffer and printing it to serial and SD-card
-   recvLen = sizeof(canDataBuf);
-   if(lora.recv(canDataBuf, &recvLen))                     // Recieving the buffer from CarTransmitter
-     {
-       Serial.println(F("-----------------")); 
-       Serial.println(F("Got CAN messages: "));
-       canDataLog.println("-----------------"); 
-       canDataLog.println("Got CAN messages: ");
-       for(byte i = 0; i < recvLen; i++)                    // Printing the buffer to the serial and SD-Card
-          {
-            Serial.print("Position ");
-            Serial.print(i);
-            Serial.print(" of the buffer is: ");
-            Serial.println(canDataBuf[i], HEX);
-              
-            canDataLog.print("Position ");
-            canDataLog.print(i); 
-            canDataLog.print(" of the buffer is: ");         
-            canDataLog.println(canDataBuf[i], HEX);             
-           }
-           
-       Serial.println(F("-----------------"));
-       canDataLog.println("-----------------");  
-       delay(100);         
-      } 
+     Serial.println(F("LoRa is available"));
+     Serial.println(F("The file is also available"));
+     
+     // Collecting the recieved data in a buffer and printing it to serial and SD-card
+     recvLen = sizeof(canDataBuf);
+     if(lora.recv(canDataBuf, &recvLen))                     // Recieving the buffer from CarTransmitter
+       {
+         Serial.println(F("-----------------")); 
+         Serial.println(F("Got CAN messages: "));
+         canDataLog.println("-----------------"); 
+         canDataLog.println("Got CAN messages: ");
+         for(byte i = 0; i < recvLen; i++)                    // Printing the buffer to the serial and SD-Card
+            {
+              Serial.print("Position ");
+              Serial.print(i);
+              Serial.print(" of the buffer is: ");
+              Serial.println(canDataBuf[i], HEX);
+                
+              canDataLog.print("Position ");
+              canDataLog.print(i); 
+              canDataLog.print(" of the buffer is: ");         
+              canDataLog.println(canDataBuf[i], HEX);             
+            }             
+         Serial.println(F("-----------------"));
+         canDataLog.println("-----------------");  
+         delay(100);         
+       } 
+     canDataLog.close();
+     delay(100);
+  }
+  else
+  {
+    Serial.println(F("LoRa was not available"));
     canDataLog.close();
     delay(100);
-    }
-    else
-    {
-      //Serial.println("LoRa was not available");
-      canDataLog.close();
-      delay(100);
-    }
-  
+  }  
 } 
      
 
